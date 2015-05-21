a32e18a5ff4c37be0d3784ce14feb5ec59888006
Bella <woo.bella@gmail.com>
2015-05-21 15:51:44 -0400

    includes most features besides getting a correct guess

--- hangman.rb
+++ hangman.rb
@@ -4,30 +4,53 @@ require '/.player.rb'
 
 class HangTheHooman
 	attr_reader :answer
+
 	def initialize
-		puts "Let's play Hangman.\nGenerating your word..."
-		# Select a random word from the Dictiionary array
+		# Select a random word from the Dictionary array
 		@answer = dictionary.sample
+		@right_guess = " "
 		@user_has_won = false
 		@tries = answer.length * 2
+		puts answer.each do |x|
+			print "_"
+		end
+		@guess = gets.chomp.downcase
 	end
 
-	def over?
+	# Transform the answer into an array of letters
+	def letters_in_word
+		letters = answer.chars.to_a
 	end
 
-	def won?
-	end
-
-	def lost?
-	end
-
-# Transform the answer into an array of letters
-
-# Check to see if the letter is in the array
-# If letter is in array, remove from array
+	# Check to see if the letter is in the array
+	# If the letter is not in the word, 
 	def check_letter guess
-		until [].include(guess)
+		until letters.include(guess)
+			@tries -= 1
+			return :yup!
+			puts "#{guess} isn't in the word I'm thinking of. Try again."
+		end
+
+		if letters.include?(guess)
+			@tries -= 1
+		end
 	end
 
-end
+# Game is over if human player has won or lost
+	def over?
+		won? || lost?
+	end
+
+# Human wins when this is true
+	def won?
+		@user_has_won
+	end
+
+# Human has lost when human runs out of tries AND the word is not completely filled out
+	def lost?
+		@tries.zero? && !@user_has_won
+	end
+
+	end
+
 
--- player.rb
+++ player.rb
@@ -5,12 +5,20 @@ game = HangTheHooman.new
 
 class SillyHooman
 	def get_letter
-		puts "Guess a letter to complete spelling the word"
-		gets.chomp
+		puts "Let's play Hangman.\nTry guessing what this word is:"
+		gets.chomp.downcase
 	end
 end
 
+# Controls asking human player to keep guessing
 until game.over?
 	guess = SillyHooman.get_letter
 	result = game.check_letter guess
-	
+end
+
+if game.won?
+	puts "Wahoo! You guessed the word."
+elsif game.lost?
+	puts "This is really sad. You couldn't guess the word"
+	puts "By the way, the word was @{game.answer}."
+end
